c7b5ff0ef56049cda5e98094c17cc39c2336127b
Noisome Possum <contact@michaelshaughnessy.net>
2017-09-11 15:05:22 +0200

    Remove '-' from var name for jquery-ui

--- tests/unit/Pathfinding.js
+++ tests/unit/Pathfinding.js
@@ -2,7 +2,7 @@ define(function (require) {
     var registerSuite = require('intern!object');
     var assert = require('intern/chai!assert');
     var jquery = require('libs/jquery/jquery.min.js');
-    var jquery-ui = require('libs/jquery-ui/jquery-ui.min.js');
+    var jqueryui = require('libs/jquery-ui/jquery-ui.min.js');
     var howler = require('libs/howler.min.js');
     var Player = require('src/game/game/Player');
 
